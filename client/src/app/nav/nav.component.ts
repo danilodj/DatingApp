65f0cc784354d8391a6cd437c505cedd07de0854
Danilo <djordjevic.danilo84@gmail.com>
2024-02-16 20:54:54 +0100

    feature/Implemented Error handling (#5)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,10 +22,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         this.router.navigateByUrl("/members");
-      },
-      error: (err) => {
-        console.error(err);
-        this.toastr.error(err.error);
       }
     })
   }
